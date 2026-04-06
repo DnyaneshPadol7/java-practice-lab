8d022f857a9b7bab725873831ff50fa386c12335
The-Viki <dpadol14@gmail.com>
2026-04-06 14:50:00 +0530

    this is my first code

---
+++ code/ConstBMI.java
@@ -0,0 +1,37 @@
+/*
+public class ConstBMI {
+  // Properties 
+	// height // Weight
+	float height;
+	float weight;
+	public ConstBMI() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public ConstBMI(float height, float weight) {
+		super();
+		this.height = height;
+		this.weight = weight;
+	}
+	
+	float getBMI(){
+		return (weight/(height*height)); 
+	}
+	
+	
+	
+	 void showBMI() {
+		 float bmi = getBMI();
+		if (bmi <= 18.4f) {
+			System.out.println("Your BMI is "+bmi+" You are Underweight");
+		} else if (bmi >= 18.5f && bmi<=22.9f) {
+			System.out.println("Your BMI is "+bmi+" You are Normal");
+		} else if (bmi>=23.0f && bmi<=24.9f) {
+			System.out.println("Your BMI is "+bmi+" You are Overweight");
+		} else  {
+			System.out.println("Your BMI is "+bmi+" You are just Obese");
+		}
+	}
+}
+
+*/
